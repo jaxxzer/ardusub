c4bf07bb088df38e81d0acb4c803f2e84ad09d59
Jacob Walser <jwalser90@gmail.com>
2016-10-01 00:04:54 -0400

    Sub: recover rangefinder status in depth hold mode

--- ArduSub/control_althold.cpp
+++ ArduSub/control_althold.cpp
@@ -89,7 +89,9 @@ void Sub::althold_run()
 	}
 
 	// adjust climb rate using rangefinder
-	if (rangefinder_alt_ok()) {
+	if(rangefinder_state.enabled && rangefinder.status() == RangeFinder::RangeFinder_OutOfRangeLow) {
+		target_climb_rate = wp_nav.get_speed_up();
+	} else if (rangefinder_alt_ok()) {
 		// if rangefinder is ok, use surface tracking
 		target_climb_rate = get_surface_tracking_climb_rate(target_climb_rate, pos_control.get_alt_target(), G_Dt);
 	}
