51c3b316d5729d622e7829f1d879e581ea7785d7
Jacob Walser <jwalser90@gmail.com>
2017-02-07 14:43:01 -0500

    AP_LeakDetector: New library and analog/digital sensor drivers

---
+++ libraries/AP_LeakDetector/AP_LeakDetector_Backend.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "AP_LeakDetector.h"
+
+class AP_LeakDetector_Backend {
+public:
+    AP_LeakDetector_Backend(AP_LeakDetector &_leak_detector, AP_LeakDetector::LeakDetector_State &_state);
+    virtual void read(void) = 0;
+
+protected:
+    AP_LeakDetector &leak_detector;
+    AP_LeakDetector::LeakDetector_State &state;
+};
