f39d8552a128548123a7878a061847cb775e1cee
Jacob Walser <jwalser90@gmail.com>
2017-02-20 10:43:44 -0500

    Better depth hold

--- ArduSub/control_acro.cpp
+++ ArduSub/control_acro.cpp
@@ -9,7 +9,15 @@
 bool Sub::acro_init(bool ignore_checks)
 {
     // set target altitude to zero for reporting
-    pos_control.set_alt_target(0);
+    // initialize vertical speeds and leash lengths
+    // sets the maximum speed up and down returned by position controller
+    pos_control.set_speed_z(-g.pilot_velocity_z_max, g.pilot_velocity_z_max);
+    pos_control.set_accel_z(g.pilot_accel_z);
+
+    // initialise position and desired velocity
+    pos_control.set_alt_target(inertial_nav.get_altitude());
+    pos_control.set_desired_velocity_z(inertial_nav.get_velocity_z());
+    last_pilot_heading = ahrs.yaw_sensor;
 
     return true;
 }
@@ -18,30 +26,69 @@ bool Sub::acro_init(bool ignore_checks)
 // should be called at 100hz or more
 void Sub::acro_run()
 {
-    float target_roll, target_pitch, target_yaw;
+
+    uint32_t tnow = AP_HAL::millis();
+    float target_roll, target_pitch;
+    float target_yaw_rate;
 
     // if not armed set throttle to zero and exit immediately
     if (!motors.armed() || !motors.get_interlock()) {
         motors.set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
         attitude_control.set_throttle_out_unstabilized(0,true,g.throttle_filt);
+        last_pilot_heading = ahrs.yaw_sensor;
         return;
     }
 
     motors.set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
 
-    // convert the input to the desired body frame rate
-    get_pilot_desired_angle_rates(channel_roll->get_control_in(), channel_pitch->get_control_in(), channel_yaw->get_control_in(), target_roll, target_pitch, target_yaw);
+    // apply SIMPLE mode transform to pilot inputs
+    update_simple_mode();
 
-    // run attitude controller
-    attitude_control.input_rate_bf_roll_pitch_yaw(target_roll, target_pitch, target_yaw);
+    // convert pilot input to lean angles
+    // To-Do: convert get_pilot_desired_lean_angles to return angles as floats
+    get_pilot_desired_lean_angles(channel_roll->get_control_in(), channel_pitch->get_control_in(), target_roll, target_pitch, aparm.angle_max);
 
-    // output pilot's throttle without angle boost
-    attitude_control.set_throttle_out(channel_throttle->norm_input(), false, g.throttle_filt);
+    // get pilot's desired yaw rate
+    target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
 
-    //control_in is range 0-1000
+    // call attitude controller
+    // update attitude controller targets
+
+    if (!is_zero(target_yaw_rate)) { // call attitude controller with rate yaw determined by pilot input
+        attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate, get_smoothing_gain());
+        last_pilot_heading = ahrs.yaw_sensor;
+        last_pilot_yaw_input_ms = tnow; // time when pilot last changed heading
+
+    } else { // hold current heading
+
+        // this check is required to prevent bounce back after very fast yaw maneuvers
+        // the inertia of the vehicle causes the heading to move slightly past the point when pilot input actually stopped
+        if (tnow < last_pilot_yaw_input_ms + 250) { // give 250ms to slow down, then set target heading
+            target_yaw_rate = 0;  // Stop rotation on yaw axis
+
+            // call attitude controller with target yaw rate = 0 to decelerate on yaw axis
+            attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate, get_smoothing_gain());
+            last_pilot_heading = ahrs.yaw_sensor; // update heading to hold
+
+        } else { // call attitude controller holding absolute absolute bearing
+            attitude_control.input_euler_angle_roll_pitch_yaw(target_roll, target_pitch, last_pilot_heading, true, get_smoothing_gain());
+        }
+    }
+
+    if (fabs(channel_throttle->norm_input()-0.5) > 0.075) {
+        // output pilot's throttle
+        attitude_control.set_throttle_out(channel_throttle->norm_input(), false, g.throttle_filt);
+        pos_control.relax_alt_hold_controllers(motors.get_throttle_hover());
+    } else {
+        pos_control.update_z_controller();
+    }
+
+
+    //control_in is range -1000-1000
     //radio_in is raw pwm value
     motors.set_forward(channel_forward->norm_input());
     motors.set_lateral(channel_lateral->norm_input());
+
 }
 
 
